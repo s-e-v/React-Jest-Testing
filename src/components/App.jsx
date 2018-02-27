e9ddeb750fae085d8b66c5f065d5d55e29178617
s-e-v <severino.gurashi@gmail.com>
2018-02-27 16:55:54 +0000

    Flow version fix and type checking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,14 @@ import './App.css';
 import Grid from './listings/Grid';
 import courses from '../data/courses.json';
 
+
 export default class App extends Component {
-  constructor(props) {
+
+  state = {
+    courses: Array
+  }
+
+  constructor(props: any) {
     super(props);
     this.state = { courses };
   }
@@ -17,16 +23,11 @@ export default class App extends Component {
           <nav className="blue lighten-2">
             <div className="nav-wrapper">
               <a href="#" className="brand-logo center">Courses</a>
-              {/* <ul id="nav-mobile" className="left hide-on-med-and-down">
-                <li><a href="#">React</a></li>
-                <li><a href="#">React Native</a></li>
-                <li><a href="#">Framer</a></li>
-              </ul> */}
             </div>
           </nav>
         </div>
         <div>
-          <Grid items={this.state.courses} />
+            <Grid items={this.state.courses}/>
         </div>
       </div>
     );
